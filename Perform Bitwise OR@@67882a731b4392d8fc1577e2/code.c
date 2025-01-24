ae1c113de7fc4aea9a2ab33717519b191027b5ce
Saavi2476 <saaviverma2006@gmail.com>
2025-01-24 19:06:01 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main(){
     unsigned int a,b;
-    print("");
     scanf("%u",&a);
-    printf("");
     scanf("%u",&b);
-    printf("%u",a|b);
+    printf("%u \n",a|b);
     return 0;
 }
