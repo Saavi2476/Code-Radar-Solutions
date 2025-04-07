b811e337e299de530da37a1a09e135cbf1f53edd
Saavi Verma <saaviverma2006@gmail.com>
2025-04-07 21:00:09 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,36 +1,76 @@
+// #include<stdio.h>
+// int main(){
+//     int n,i;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     if(n==1){
+//         printf("%d\n",arr[0]);
+//         return 0;
+//     }
+//     for(i=0;i<n;i++){
+//         if(i==0){
+//             if(arr[i]>arr[i+1]){
+//                 printf("%d\n",arr[i]);
+//                 return 0;
+//             }
+//         }
+//         else if(i==n-1){
+//             if(arr[i]>arr[i-1]){
+//                 printf("%d\n",arr[i]);
+//             }
+//             return 0;
+//         }
+//         else{
+//             if(arr[i]>arr[i+1]&&arr[i]>arr[i-1]){
+//                 printf("%d\n",arr[i]);
+//             }
+//             return 0;
+//         }
+//     }
+//     printf("-1\n");
+//     return 0;
+    
+// }
 #include<stdio.h>
-int main(){
-    int n,i;
-    scanf("%d",&n);
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
     int arr[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    if(n==1){
-        printf("%d\n",arr[0]);
+
+    if (n == 1) {
+        printf("%d\n", arr[0]);
         return 0;
     }
-    for(i=0;i<n;i++){
-        if(i==0){
-            if(arr[i]>arr[i+1]){
-                printf("%d\n",arr[i]);
+
+    for (i = 0; i < n; i++) {
+        if (i == 0) {
+            if (arr[i] > arr[i + 1]) {
+                printf("%d\n", arr[i]);
+                return 0;
+            }
+        } else if (i == n - 1) {
+            if (arr[i] > arr[i - 1]) {
+                printf("%d\n", arr[i]);
+                return 0;
+            }
+        } else {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                printf("%d\n", arr[i]);
                 return 0;
             }
         }
-        else if(i==n-1){
-            if(arr[i]>arr[i-1]){
-                printf("%d\n",arr[i]);
-            }
-            return 0;
-        }
-        else{
-            if(arr[i]>arr[i+1]&&arr[i]>arr[i-1]){
-                printf("%d\n",arr[i]);
-            }
-            return 0;
-        }
     }
+
+    // If no peak found
     printf("-1\n");
     return 0;
-    
 }
+
